581a78d94405243f1802d93bd5cf1d55aa735839
= <pshutchinson@hotmail.com>
2020-11-02 22:22:55 +0000

    Exercise 1 changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -8,8 +10,15 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    #return 'Hello World!'
+    items = get_items()
+    return render_template('/index.html', items=items)
 
+@app.route('/add', methods = ['POST'])
+def add():
+    add_item(request.form.get('title'))
+    items = get_items()
+    return render_template('/index.html', items=items)
 
 if __name__ == '__main__':
     app.run()
