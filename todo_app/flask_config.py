f1f78ba60860d0f916da8001d61171f1e6ad5cfe
= <pshutchinson@hotmail.com>
2021-07-11 00:09:40 +0100

    Updating flask_config

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -5,7 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     def __init__(self):
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
+        self.SECRET_KEY = os.getenv('SECRET_KEY')
+        #self.SECRET_KEY = os.environ.get('SECRET_KEY')
         self.TRELLO_KEY= os.environ.get('TRELLO_KEY')
         self.TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
         self.TRELLO_BOARD_ID = os.environ.get('TRELLO_BOARD_ID')
